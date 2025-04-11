cc3f8a8f1051c91947c880ee39e850bcd81067dd
Evelynn McKay <50677746+Frand0@users.noreply.github.com>
2025-04-11 17:01:20 -0400

    complete and mostly functional game

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-
+const MAX_COMPUTER_SPEED = 2;
 
 // the ball
 const BALL_SIZE = 5;
-let ballPosition = { x: 20, y: 30 };
+let ballPosition;
 
-let xSpeed = 4;
-let ySpeed = 2;
+let xSpeed;
+let ySpeed;
+
+function initBall() {
+    ballPosition = { x: 100, y: 50 };
+    xSpeed = 4;
+    ySpeed = 2;
+}
 
 //paddles
 
@@ -21,6 +27,13 @@ const PADDLE_OFFSET = 10;
 
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
+
+//scoring
+
+let leftScore = 0;
+let rightScore = 0;
+
+let gameOver = false;
 
 document.addEventListener("mousemove", e => {
     rightPaddleTop = e.y - canvas.offsetTop;
@@ -32,7 +45,7 @@ function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, width, height);
 
-    // The rrest remains white
+    // The rest remains white
     ctx.fillStyle = "white";
 
     // Draw ball
@@ -52,11 +65,39 @@ function draw() {
         PADDLE_WIDTH,
         PADDLE_HEIGHT
     );
+
+    //Draw scores
+    ctx.font = "30px monospace";
+    ctx.textAlign = "left";
+    ctx.fillText(leftScore.toString(), 50, 50);
+    ctx.textAlign = "right";
+    ctx.fillText(rightScore.toString(), width - 50, 50);
 };
+
+// left paddle opponent follows top and bottom points of ball
+
+function followBall() {
+    let ball = {
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE
+    };
+    let leftPaddle = {
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT
+    };
+
+    if (ball.top < leftPaddle.top) {
+        leftPaddleTop -= MAX_COMPUTER_SPEED;
+    } else if (ball.bottom > leftPaddle.bottom) {
+        leftPaddleTop += MAX_COMPUTER_SPEED;
+    };
+};
+
 
 function update() {
     ballPosition.x += xSpeed;
     ballPosition.y += ySpeed;
+    followBall();
 };
 
 function checkPaddleCollision(ball, paddle) {
@@ -68,6 +109,21 @@ function checkPaddleCollision(ball, paddle) {
         ball.bottom > paddle.top
     );
 };
+
+//adjust angle the ball bounces back at if it hits top or bottom of paddle
+
+function adjustAngle(distanceFromTop, distanceFromBottom) {
+    if (distanceFromTop < 0) {
+        // ball hits top of paddle, reduce ySpeed
+        console.log("Top hit!");
+        ySpeed -= 0.5;
+    } else if (distanceFromBottom < 0) {
+        // ball hits bottom of paddle, increase ySpeed
+        console.log("Bottom hit!");
+        ySpeed += 0.5;
+    };
+};
+
 
 // check when ball collides and reverse speed
 function checkCollision() {
@@ -94,67 +150,62 @@ function checkCollision() {
 
     if (checkPaddleCollision(ball, leftPaddle)) {
         //left p collision
+        let distanceFromTop = ball.top - leftPaddle.top;
+        let distanceFromBottom = leftPaddle.bottom - ball.bottom;
+        adjustAngle(distanceFromTop, distanceFromBottom);
         xSpeed = Math.abs(xSpeed);
     }
 
     if (checkPaddleCollision(ball, rightPaddle)) {
         //right p collision
+        let distanceFromTop = ball.top - rightPaddle.top;
+        let distanceFromBottom = rightPaddle.bottom - ball.bottom;
+        adjustAngle(distanceFromTop, distanceFromBottom);
         xSpeed = -Math.abs(xSpeed);
-    }
+    };
+
+    if (ball.left < 0) {
+        rightScore++;
+        initBall();
+    };
+
+    if (ball.right > width) {
+        leftScore++;
+        initBall();
+    };
+
+    if (leftScore > 9 || rightScore > 9) {
+        gameOver = true;
+    };
 
     if (ball.left < 0 || ball.right > width) {
         xSpeed = -xSpeed;
     };
+
     if (ball.top < 0 || ball.bottom > height) {
         ySpeed = -ySpeed;
     };
 };
 
-ballMove = false;
+function drawGameOver() {
+    ctx.fillStyle = "white";
+    ctx.font = "30px monospace";
+    ctx.textAlign = "center";
+    ctx.fillText("GAME OVER", width / 2, height / 2);
+}
 
 function gameLoop() {
     draw();
     update();
     checkCollision();
-    // call again after timeout
-    setTimeout(gameLoop, 30);
-    if (ballMove == true) {
-        xSpeed = 0;
-        ySpeed = 0;
+
+    if (gameOver) {
+        draw();
+        drawGameOver();
+    } else {
+        setTimeout(gameLoop, 25);
     };
 };
 
-function resetGame() {
-    xSpeed = 4;
-    ySpeed = 2;
-    ballMove = false;
-    clearTimeout(gameLoop);
-    ctx.clearRect(0, 0, width, height);
-};
-
-
-
-//Start, pause buttons
-
-document.querySelector("#startGame").addEventListener("click", e => {
-    resetGame();
-    gameLoop();
-});
-
-
-document.querySelector("#pauseGame").addEventListener("click", e => {
-    ballMove = true;
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+initBall();
+gameLoop();
